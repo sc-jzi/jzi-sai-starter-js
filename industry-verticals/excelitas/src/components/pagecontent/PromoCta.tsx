10d2a29d8bf908fa14b7d745b3d89f55a0e84b8a
jzi <jzi@sitecore.net>
2026-06-18 16:14:27 -0400

    Added variant

--- industry-verticals/excelitas/src/components/pagecontent/PromoCta.tsx
+++ industry-verticals/excelitas/src/components/pagecontent/PromoCta.tsx
@@ -74,15 +74,71 @@ export const Default = (props: PromoCtaProps): JSX.Element => {
               <DottedAccent className="dotted-accent-top" />
               <NextImage
                 field={props.fields.Image}
-                className={`d-block mx-lg-auto img-fluid ${
-                  !isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
-                }`}
+                className={`d-block mx-lg-auto img-fluid ${!isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
+                  }`}
                 width={900}
                 height={900}
               />
               <DottedAccent className="dotted-accent-bottom" />
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const ImageLeft = (props: PromoCtaProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const { page } = useSitecore();
+  const isPageEditing = page.mode.isEditing;
+  const [isVisible, domRef] = useVisibility();
+  const sxaStyles = `${props.params?.styles || ''}`;
+
+  return (
+    <div className={`component promo-cta ${sxaStyles}`} id={id ? id : undefined} ref={domRef}>
+      <div className="container">
+        <div className="row row-gap-4 main-content align-items-center">
+          <div className="col-md-10 mx-auto col-lg-7 mx-lg-0">
+            <div className="image-wrapper">
+              <DottedAccent className="dotted-accent-top" />
+              <NextImage
+                field={props.fields.Image}
+                className={`d-block mx-lg-auto img-fluid ${!isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
+                  }`}
+                width={900}
+                height={900}
+              />
+              <DottedAccent className="dotted-accent-bottom" />
+            </div>
+          </div>
+          <div className="col-lg-5 text-center text-lg-start">
+            <h6 className="eyebrow-accent">
+              <Text field={props.fields.Eyebrow} />
+            </h6>
+            <h1 className="display-6 fw-bold mb-3">
+              <Text field={props.fields.Title} />
+            </h1>
+            <div className="promo-cta-text">
+              <p className="fs-5">
+                <Text field={props.fields.Subtitle} />
+              </p>
+
+              <RichText field={props.fields.Text} className="text-content" />
+
+              <div className="row mt-2">
+                <Placeholder name="promo-cta" rendering={props.rendering} />
+              </div>
+
+              {(isPageEditing || props.fields?.Link?.value?.href) && (
+                <Link field={props.fields.Link} className="button button-main mt-3 me-4" />
+              )}
+              {(isPageEditing || props.fields?.Link2?.value?.href) && (
+                <Link field={props.fields.Link2} className="button button-simple mt-3 " />
+              )}
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
@@ -141,9 +197,8 @@ export const WithPlaceholderColumn = (props: PromoCtaProps): JSX.Element => {
                 <DottedAccent className="dotted-accent-top" />
                 <NextImage
                   field={props.fields.Image}
-                  className={`d-block mx-lg-auto img-fluid ${
-                    !isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
-                  }`}
+                  className={`d-block mx-lg-auto img-fluid ${!isPageEditing ? `fade-section ${isVisible ? 'is-visible' : ''}` : ''
+                    }`}
                   width={900}
                   height={900}
                 />
