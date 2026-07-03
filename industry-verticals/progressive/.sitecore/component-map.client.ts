c4eec2ad96c43b406666e819390bcdb5f9b104bc
jzi <jzi@sitecore.net>
2026-07-03 11:15:57 -0400

    Initial commit

---
+++ industry-verticals/progressive/.sitecore/component-map.client.ts
@@ -0,0 +1,137 @@
+// Client-safe component map for App Router
+
+import { BYOCClientWrapper, NextjsContentSdkComponent, FEaaSClientWrapper } from '@sitecore-content-sdk/nextjs';
+import { Form } from '@sitecore-content-sdk/nextjs';
+
+import * as ThemeSwitcher from 'src/components/utilities/ThemeSwitcher';
+import * as SiteTheme from 'src/components/utilities/SiteTheme';
+import * as LoanCalculator from 'src/components/utilities/LoanCalculator';
+import * as LanguageSwitcher from 'src/components/utilities/LanguageSwitcher';
+import * as ContactForm from 'src/components/utilities/ContactForm';
+import * as ApplicationForm from 'src/components/utilities/ApplicationForm';
+import * as SuggestionBlock from 'src/components/search/SuggestionBlock';
+import * as SortOrder from 'src/components/search/SortOrder';
+import * as SearchResultsComponent from 'src/components/search/SearchResultsComponent';
+import * as SearchResults from 'src/components/search/SearchResults';
+import * as SearchProvider from 'src/components/search/SearchProvider';
+import * as SearchPagination from 'src/components/search/SearchPagination';
+import * as SearchFacets from 'src/components/search/SearchFacets';
+import * as ResultsPerPage from 'src/components/search/ResultsPerPage';
+import * as QuestionsAnswers from 'src/components/search/QuestionsAnswers';
+import * as QueryResultsSummary from 'src/components/search/QueryResultsSummary';
+import * as PreviewSearch from 'src/components/search/PreviewSearch';
+import * as HomeHighlighted from 'src/components/search/HomeHighlighted';
+import * as Filter from 'src/components/search/Filter';
+import * as CardViewSwitcher from 'src/components/search/CardViewSwitcher';
+import * as ArticleHorizontalCard from 'src/components/search/ArticleHorizontalCard';
+import * as ArticleCard from 'src/components/search/ArticleCard';
+import * as ColumnSplitter from 'src/components/pagestructure/ColumnSplitter';
+import * as TwoColumnCta from 'src/components/pagecontent/TwoColumnCta';
+import * as ThreeColumnCta from 'src/components/pagecontent/ThreeColumnCta';
+import * as Testimonials from 'src/components/pagecontent/Testimonials';
+import * as StatsCounter from 'src/components/pagecontent/StatsCounter';
+import * as RichText from 'src/components/pagecontent/RichText';
+import * as Quote from 'src/components/pagecontent/Quote';
+import * as Questions from 'src/components/pagecontent/Questions';
+import * as PromoCta from 'src/components/pagecontent/PromoCta';
+import * as ProjectList from 'src/components/pagecontent/ProjectList';
+import * as ProjectDetails from 'src/components/pagecontent/ProjectDetails';
+import * as ParallaxBanner from 'src/components/pagecontent/ParallaxBanner';
+import * as PageBackground from 'src/components/pagecontent/PageBackground';
+import * as ImageGallery from 'src/components/pagecontent/ImageGallery';
+import * as HeroBanner from 'src/components/pagecontent/HeroBanner';
+import * as Hero from 'src/components/pagecontent/Hero';
+import * as HeadingCta from 'src/components/pagecontent/HeadingCta';
+import * as FourColumnCta from 'src/components/pagecontent/FourColumnCta';
+import * as FiveColumnCta from 'src/components/pagecontent/FiveColumnCta';
+import * as Features from 'src/components/pagecontent/Features';
+import * as DocumentsList from 'src/components/pagecontent/DocumentsList';
+import * as CtaBanner from 'src/components/pagecontent/CtaBanner';
+import * as Comparison from 'src/components/pagecontent/Comparison';
+import * as Carousel from 'src/components/pagecontent/Carousel';
+import * as AuthorWidget from 'src/components/pagecontent/AuthorWidget';
+import * as AuthorList from 'src/components/pagecontent/AuthorList';
+import * as AuthorDetails from 'src/components/pagecontent/AuthorDetails';
+import * as ArticleList from 'src/components/pagecontent/ArticleList';
+import * as ArticleDetails from 'src/components/pagecontent/ArticleDetails';
+import * as AppPromo from 'src/components/pagecontent/AppPromo';
+import * as Accordion from 'src/components/pagecontent/Accordion';
+import * as ParallaxBackgroundImage from 'src/components/non-sitecore/ParallaxBackgroundImage';
+import * as IconAccent from 'src/components/non-sitecore/IconAccent';
+import * as DottedAccent from 'src/components/non-sitecore/DottedAccent';
+import * as CountUp from 'src/components/non-sitecore/CountUp';
+import * as Navigation from 'src/components/navigation/Navigation';
+import * as Header from 'src/components/navigation/Header';
+import * as Footer from 'src/components/navigation/Footer';
+import * as Eyebrow from 'src/components/navigation/Eyebrow';
+import * as Breadcrumb from 'src/components/navigation/Breadcrumb';
+
+export const componentMap = new Map<string, NextjsContentSdkComponent>([
+  ['BYOCWrapper', BYOCClientWrapper],
+  ['FEaaSWrapper', FEaaSClientWrapper],
+  ['Form', Form],
+  ['ThemeSwitcher', { ...ThemeSwitcher }],
+  ['SiteTheme', { ...SiteTheme }],
+  ['LoanCalculator', { ...LoanCalculator }],
+  ['LanguageSwitcher', { ...LanguageSwitcher }],
+  ['ContactForm', { ...ContactForm }],
+  ['ApplicationForm', { ...ApplicationForm }],
+  ['SuggestionBlock', { ...SuggestionBlock }],
+  ['SortOrder', { ...SortOrder }],
+  ['SearchResultsComponent', { ...SearchResultsComponent }],
+  ['SearchResults', { ...SearchResults }],
+  ['SearchProvider', { ...SearchProvider }],
+  ['SearchPagination', { ...SearchPagination }],
+  ['SearchFacets', { ...SearchFacets }],
+  ['ResultsPerPage', { ...ResultsPerPage }],
+  ['QuestionsAnswers', { ...QuestionsAnswers }],
+  ['QueryResultsSummary', { ...QueryResultsSummary }],
+  ['PreviewSearch', { ...PreviewSearch }],
+  ['HomeHighlighted', { ...HomeHighlighted }],
+  ['Filter', { ...Filter }],
+  ['CardViewSwitcher', { ...CardViewSwitcher }],
+  ['ArticleHorizontalCard', { ...ArticleHorizontalCard }],
+  ['ArticleCard', { ...ArticleCard }],
+  ['ColumnSplitter', { ...ColumnSplitter }],
+  ['TwoColumnCta', { ...TwoColumnCta }],
+  ['ThreeColumnCta', { ...ThreeColumnCta }],
+  ['Testimonials', { ...Testimonials }],
+  ['StatsCounter', { ...StatsCounter }],
+  ['RichText', { ...RichText }],
+  ['Quote', { ...Quote }],
+  ['Questions', { ...Questions }],
+  ['PromoCta', { ...PromoCta }],
+  ['ProjectList', { ...ProjectList }],
+  ['ProjectDetails', { ...ProjectDetails }],
+  ['ParallaxBanner', { ...ParallaxBanner }],
+  ['PageBackground', { ...PageBackground }],
+  ['ImageGallery', { ...ImageGallery }],
+  ['HeroBanner', { ...HeroBanner }],
+  ['Hero', { ...Hero }],
+  ['HeadingCta', { ...HeadingCta }],
+  ['FourColumnCta', { ...FourColumnCta }],
+  ['FiveColumnCta', { ...FiveColumnCta }],
+  ['Features', { ...Features }],
+  ['DocumentsList', { ...DocumentsList }],
+  ['CtaBanner', { ...CtaBanner }],
+  ['Comparison', { ...Comparison }],
+  ['Carousel', { ...Carousel }],
+  ['AuthorWidget', { ...AuthorWidget }],
+  ['AuthorList', { ...AuthorList }],
+  ['AuthorDetails', { ...AuthorDetails }],
+  ['ArticleList', { ...ArticleList }],
+  ['ArticleDetails', { ...ArticleDetails }],
+  ['AppPromo', { ...AppPromo }],
+  ['Accordion', { ...Accordion }],
+  ['ParallaxBackgroundImage', { ...ParallaxBackgroundImage }],
+  ['IconAccent', { ...IconAccent }],
+  ['DottedAccent', { ...DottedAccent }],
+  ['CountUp', { ...CountUp }],
+  ['Navigation', { ...Navigation }],
+  ['Header', { ...Header }],
+  ['Footer', { ...Footer }],
+  ['Eyebrow', { ...Eyebrow }],
+  ['Breadcrumb', { ...Breadcrumb }],
+]);
+
+export default componentMap;
---
+++ industry-verticals/progressive/src/app/[site]/[locale]/[[...path]]/layout.tsx
@@ -0,0 +1,20 @@
+import { setCachedPageParams } from '@sitecore-content-sdk/nextjs';
+import { SearchProvider } from 'components/search/SearchProvider';
+
+export default async function SiteLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ site: string; locale: string }>;
+}) {
+  const { site, locale } = await params;
+
+  setCachedPageParams({ locale, site });
+
+  return (
+    <SearchProvider locale={locale}>
+      {children}
+    </SearchProvider>
+  );
+}
